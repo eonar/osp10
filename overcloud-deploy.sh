619fb1fc549b5ee2995af599502de63db5c8e1ea
yk <eonar@techwikilabsasia.local>
2018-02-08 11:22:52 +0800

    ovs-dpdk ready for deployment

--- overcloud-deploy.sh
+++ overcloud-deploy.sh
@@ -5,4 +5,9 @@ openstack overcloud deploy \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/neutron-ovs-dpdk.yaml \
 -e ./network-environment.yaml \
---log-file overcloud_install.log &> overcloud_install.log
+--stack tmrnd
+--log-file overcloud-install.log &> overcloud-install.log
+#--neutron-disable-tunneling \ 
+#-r /home/stack/templates/roles-data.yaml \
+#-e /home/stack/templates/bigswitch-config-p.yaml \
+#-e /home/stack/templates/environments/neutron-ml2-bigswitch.yaml \
